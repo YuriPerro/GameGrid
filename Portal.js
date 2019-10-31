65d33bcb785bbd3fee6f44b68d1d4e8414731d36
Yuri Perro <yuribaumgartner@ice.ufjf.br>
2019-10-31 20:44:31 -0300

    mudando algumas coisas

--- Portal.js
+++ Portal.js
@@ -1,7 +1,19 @@
 function Portal(params = {}) {
     var exemplo = {
-        x: 150,    y: 150,
+        x: 2000,    y: 18*32,
+        vx: 0,  vy: 0,
+        ax: 0,  ay: 0,
         w: 25,  h: 32,   
+        a: 0,
+        va: 0,    vm: 0,
+        frame: 0,
+        props: {},
+        cooldown: 0,
+        color: "red",
+        pc: null,
+        movimento: null,
+        scene: null,
+        mapa: null,
         assets: null,
     }
     Object.assign(this, exemplo, params);
@@ -10,9 +22,6 @@ Portal.prototype = new Portal();
 Portal.prototype.constructor = Portal;
 
 Portal.prototype.mover = function(dt){
-    this.frame += 12*dt;
-
-    this.checaColisao();
 }
 
 Portal.prototype.desenhar = function (ctx) {
@@ -26,8 +35,8 @@ Portal.prototype.desenhar = function (ctx) {
             96, //Corte na horizontal
             32,
             32,
-            16*32,
-            19*32,
+            this.x,
+            this.y,
             32,
             32,
         );
@@ -35,7 +44,12 @@ Portal.prototype.desenhar = function (ctx) {
 };
 
 Portal.prototype.checaColisao = function(alvo){
-    
+    if ((alvo.x + alvo.w/2 )  < this.x - this.w / 2) return false;
+    if ((alvo.x - alvo.w/2 )  > this.x + this.w / 2) return false;
+    if ((alvo.y + alvo.h/2 )  < this.y - this.h / 2) return false;
+    if ((alvo.y - alvo.h/2 )  > this.y + this.h / 2) return false;
+
+    return true;
 }
 
 
